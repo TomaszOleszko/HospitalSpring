7083dde11aff2d2ef99bfceb2cf8e322c357059e
Dawid Nalepa <dawid.nalepa@pollub.edu.pl>
2022-11-21 12:48:04 +0100

    Add: Patient entity file

--- src/main/java/com/example/hospital/entities/Patient.java
+++ src/main/java/com/example/hospital/entities/Patient.java
@@ -1,4 +1,32 @@
 package com.example.hospital.entities;
 
-public class Patient {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="Patient")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class Patient extends PersonalData implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long patientID;
+    private String pesel;
+    private String city;
+    private String country;
+    private String postalCode;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="patient", fetch = FetchType.EAGER)
+    List<Visit> visits;
+
 }
+
+
