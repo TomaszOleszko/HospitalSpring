7a51320cdf0f144f1c20d57c5ea47f41c354c34c
Dawid Nalepa <dawid.nalepa@pollub.edu.pl>
2022-11-21 12:50:48 +0100

    Add: PatientService file

--- src/main/java/com/example/hospital/services/PatientService.java
+++ src/main/java/com/example/hospital/services/PatientService.java
@@ -1,4 +1,35 @@
 package com.example.hospital.services;
 
+import com.example.hospital.dao.PatientRepository;
+import com.example.hospital.entities.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PatientService {
+    @Autowired
+    PatientRepository patRepo;
+
+    public Patient save(Patient patient){
+        return patRepo.save(patient);
+    }
+
+    public List<Patient> getAll(){
+        return patRepo.findAll();
+    }
+
+    public Patient findByPatientId(Long Id){
+        return patRepo.findById(Id).orElse(null);
+    }
+
+    public void delete(Patient patient){
+        patRepo.delete(patient);
+    }
+
+    public void deleteById(Long id){
+        patRepo.deleteById(id);
+    }
 }
+
