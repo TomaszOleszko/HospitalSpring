4bea565e3af0883d540ef71cdfe2049c11a530c9
Tomasz Oleszko <tomekoleszko@gmail.com>
2022-11-21 12:57:44 +0100

    Add: Visit service

--- src/main/java/com/example/hospital/services/VisitService.java
+++ src/main/java/com/example/hospital/services/VisitService.java
@@ -1,4 +1,36 @@
 package com.example.hospital.services;
 
+import com.example.hospital.dao.VisitRepository;
+import com.example.hospital.entities.Visit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VisitService {
+
+    @Autowired
+    VisitRepository visRepo;
+
+    public Visit save(Visit visit){
+        return visRepo.save(visit);
+    }
+
+    public List<Visit> getAll(){
+        return visRepo.findAll();
+    }
+
+    public Visit findByVisitId(Long Id){
+        return visRepo.findById(Id).orElse(null);
+    }
+
+    public void delete(Visit visit){
+        visRepo.delete(visit);
+    }
+
+    public void deleteById(Long id){
+        visRepo.deleteById(id);
+    }
 }
+
