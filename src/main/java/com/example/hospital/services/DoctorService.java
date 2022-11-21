e7b693ffdc80fe5c77f883d675d9aeed5dd90af0
BlackBushido <daniel.ogorzalek@pollub.edu.pl>
2022-11-21 12:54:01 +0100

    Add Doctor service

--- src/main/java/com/example/hospital/services/DoctorService.java
+++ src/main/java/com/example/hospital/services/DoctorService.java
@@ -1,4 +1,32 @@
 package com.example.hospital.services;
+import com.example.hospital.dao.DoctorRepository;
+import com.example.hospital.entities.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class DoctorService {
+    @Autowired
+    DoctorRepository docRepo;
+
+    public Doctor save(Doctor doctor){
+        return docRepo.save(doctor);
+    }
+
+    public List<Doctor> getAll(){
+        return docRepo.findAll();
+    }
+
+    public Doctor findByDoctorId(Long Id){
+        return docRepo.findById(Id).orElse(null);
+    }
+
+    public void delete(Doctor doctor){
+        docRepo.delete(doctor);
+    }
+    public void deleteById(Long id){
+        docRepo.deleteById(id);
+    }
 }
