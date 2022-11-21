9a179a8604d42bff9fefc45c43c200e7fe5454b2
BlackBushido <daniel.ogorzalek@pollub.edu.pl>
2022-11-21 12:52:41 +0100

    Add Doctor repository

--- src/main/java/com/example/hospital/dao/DoctorRepository.java
+++ src/main/java/com/example/hospital/dao/DoctorRepository.java
@@ -1,4 +1,14 @@
 package com.example.hospital.dao;
 
-public class DoctorRepository {
+import com.example.hospital.entities.Doctor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DoctorRepository extends CrudRepository<Doctor, Long> {
+    @Override
+    List<Doctor> findAll();
+
 }
